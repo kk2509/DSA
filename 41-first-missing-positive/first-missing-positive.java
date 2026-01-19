3fcaa7da874a34ce908ef18787ea3a274b553f27
kk2509 <114849368+kk2509@users.noreply.github.com>
2026-01-19 09:17:02 +0530

    Time: 17 ms (18.66%) | Memory: 74 MB (27.19%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,18 +1,16 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int[] filteredNums = Arrays.stream(nums).filter(n -> n > 0).toArray();
-        
-        Arrays.sort(filteredNums);
-        
+        Arrays.sort(nums);
+
         int target = 1;
-        for (int n : filteredNums) {
-            if (n == target) {
+        for (int n : nums) {
+            if (n > 0 && n == target) {
                 target++;
             } else if (n > target) {
                 return target;
             }
         }
-        
-        return target;        
+
+        return target;
     }
 }
