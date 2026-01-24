e8551672c246a5d23ea5e50f379b133d4f614907
kk2509 <114849368+kk2509@users.noreply.github.com>
2026-01-24 17:40:52 +0530

    Time: 1 ms (85.11%) | Memory: 43.2 MB (45.20%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,19 +1,19 @@
 class Solution {
-   
-        	public int numDecodings(String s) {
-        int n=s.length();
-        Integer[] mem=new Integer[n];
-        return s.length()==0?0:numDecodings(0,s,mem);      
-    }
-    private int numDecodings(int p, String s, Integer[] mem) {
-        int n=s.length();
-        if(p==n) return 1;
-        if(s.charAt(p)=='0') return 0;
-        if(mem[p]!=null) return mem[p];
-        int res=numDecodings(p+1,s,mem);
-        if(p<n-1&&(s.charAt(p)=='1'||s.charAt(p)=='2'&&s.charAt(p+1)<'7')) 
-			res+=numDecodings(p+2,s,mem);
-        return mem[p]=res;
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1]=s.charAt(0)=='0'? 0:1;
+        for(int i=2;i<=n;i++){
+            if(s.charAt(i-1)-'0'>=1){
+            dp[i]+=dp[i-1];
+            }
+            int twoDigit=(s.charAt(i-2)-'0')*10+ s.charAt(i-1)-'0';
+            if(twoDigit>=10 &&twoDigit<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
+
     }
 }
-   
